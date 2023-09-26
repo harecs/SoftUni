6808749a97bc2ed01e613351bbc02e97b5c2316d
Zahari Lazarov <harilazaro@gmail.com>
2023-09-26 12:15:59 +0300

    Finish 07. Extract Parenthesis

--- JS Advanced/09. DOM Introduction/07. Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/09. DOM Introduction/07. Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,6 @@
 function extract(content) {
+    const initialText = document.getElementById(content).textContent;
+    const parenthesis = Array.from(initialText.matchAll(/\(([\w ]+)\)/gm)).map(x => x[1]);
 
+    return parenthesis.join('; ');
 }
