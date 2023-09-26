8e9b1a8c92ee736471853cce7ed7940820a31a97
Zahari Lazarov <harilazaro@gmail.com>
2023-09-26 19:21:42 +0300

    Finish 01. Subtraction

--- JS Advanced/10. Exercise: DOM Introduction/01. Subtraction/subtract.js
+++ JS Advanced/10. Exercise: DOM Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNum = Number(document.querySelector('#firstNumber').value);
+    const secondNum = Number(document.querySelector('#secondNumber').value);
+
+    document.querySelector('#result').textContent = firstNum - secondNum;
 }
