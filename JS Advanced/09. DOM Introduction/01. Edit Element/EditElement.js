ac44e977c4e3cdea97eee169939d8c8bf60d0ba3
Zahari Lazarov <harilazaro@gmail.com>
2023-09-26 11:35:15 +0300

    Finish 01. Edit Element

--- JS Advanced/09. DOM Introduction/01. Edit Element/EditElement.js
+++ JS Advanced/09. DOM Introduction/01. Edit Element/EditElement.js
@@ -1,3 +1,9 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    let newText = element.textContent;
+
+    while (newText.includes(match)) {
+        newText = newText.replace(match, replacer);
+    }
+
+    element.textContent = newText;
 }
