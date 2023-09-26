76b43278ce481ca578b2fd24e8799a9462f2b2fe
Zahari Lazarov <harilazaro@gmail.com>
2023-09-26 20:07:07 +0300

    Finish 03. Accordion

--- JS Advanced/10. Exercise: DOM Introduction/03. Accordion/accordion.js
+++ JS Advanced/10. Exercise: DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttonElement = document.querySelector('#accordion span');
+    const extraElement = document.querySelector('#extra');
+
+    if (extraElement.style.display == 'none') {
+        extraElement.style.display = 'block';
+        buttonElement.textContent = 'Less';
+    } else {
+        extraElement.style.display = 'none';
+        buttonElement.textContent = 'More';
+    }
 }
