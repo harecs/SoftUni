e50f400fcbfbed6c053740210579e6634a22f41c
Zahari Lazarov <harilazaro@gmail.com>
2023-09-27 14:08:01 +0300

    Finish 08. Generate Report

--- JS Advanced/10. Exercise: DOM Introduction/08. Generate Report/report.js
+++ JS Advanced/10. Exercise: DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,20 @@
 function generateReport() {
-    //TODO
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    const reportArray = [];
+
+    for (const row of rows) {
+        const obj = {};
+
+        Array.from(row.children).forEach((td, i) => {
+            const checkbox = document.querySelector(`th:nth-of-type(${i + 1}) input`);
+            
+            if (checkbox.checked) {
+                obj[checkbox.getAttribute('name')] = td.textContent;
+            }
+        });
+
+        reportArray.push(obj);
+    }
+
+    document.querySelector('#output').value = JSON.stringify(reportArray);
 }
