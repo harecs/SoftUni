9431182b1d8516a074eaf88748582d5abef1c2c8
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 08:50:02 +0300

    Finish "01. List of Items"

--- JS Advanced/11. DOM Manipulations and Events/01. List-Of-Items/app.js
+++ JS Advanced/11. DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,6 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemText = document.querySelector('#newItemText').value;
+    const newLi = document.createElement('li');
+    newLi.textContent = newItemText;
+    document.querySelector('#items').appendChild(newLi);
 }
