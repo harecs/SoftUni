4155c0ae42ccc20d39199ea4972123e7722a8912
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 12:17:45 +0300

    Finish "04. Fill Dropdown"

--- JS Advanced/12. Exercise: DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/12. Exercise: DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInputElement = document.querySelector('#newItemText');
+    const valueInputElement = document.querySelector('#newItemValue');
+
+    const optionElement = document.createElement('option');
+    optionElement.textContent = textInputElement.value;
+    optionElement.value = valueInputElement.value;
+
+    document.querySelector('#menu').appendChild(optionElement);
+
+    textInputElement.value = '';
+    valueInputElement.value = '';
 }
