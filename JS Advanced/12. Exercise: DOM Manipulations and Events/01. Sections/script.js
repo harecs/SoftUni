20180222b867c91b2d729378a465dcb5a9cfcc8c
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 11:15:08 +0300

    Finish "01. Sections"

--- JS Advanced/12. Exercise: DOM Manipulations and Events/01. Sections/script.js
+++ JS Advanced/12. Exercise: DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+    const contentElement = document.querySelector('#content');
+
+    for (const word of words) {
+        const paragraphElement = document.createElement('p');
+        paragraphElement.textContent = word;
+        paragraphElement.style.display = 'none';
+
+        const divElement = document.createElement('div');
+        divElement.addEventListener('click', (e) => {
+            e.currentTarget.firstChild.style.display = 'block';
+        });
+        divElement.appendChild(paragraphElement);
+
+        contentElement.appendChild(divElement);
+    }
 }
