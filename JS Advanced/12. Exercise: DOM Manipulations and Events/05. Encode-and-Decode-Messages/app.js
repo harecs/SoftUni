421d4da4b7fcb7f632e3fb9c533d9cc586fdb1aa
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 12:39:25 +0300

    Finish "05. Encode and Decode Messages"

--- JS Advanced/12. Exercise: DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/12. Exercise: DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttonElements = document.querySelectorAll('button');
+    const textAreaElements = document.querySelectorAll('textarea');
+
+    buttonElements[0].addEventListener('click', onEncode);
+    buttonElements[1].addEventListener('click', onDecode);
+
+    function onEncode(e) {
+        const message = textAreaElements[0].value;
+        const encodedMessage = message.split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+            .join('');
+
+        textAreaElements[0].value = '';
+        textAreaElements[1].value = encodedMessage;
+    }
+
+    function onDecode(e) {
+        const message = textAreaElements[1].value;
+        const decodedMessage = message.split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) - 1))
+            .join('');
+
+        textAreaElements[1].value = decodedMessage;
+    }
 }
