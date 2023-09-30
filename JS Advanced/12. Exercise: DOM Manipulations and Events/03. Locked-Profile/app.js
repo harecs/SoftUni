bbfb3c17a7942abcf943beaf11096800e0112e0d
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 12:04:52 +0300

    Finish "03. Locked Profile"

--- JS Advanced/12. Exercise: DOM Manipulations and Events/03. Locked-Profile/app.js
+++ JS Advanced/12. Exercise: DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonElements = Array.from(document.querySelectorAll('button'));
+    buttonElements.forEach(button => button.addEventListener('click', onClick));
+
+    function onClick(e) {
+        const profileElement = e.target.parentElement;
+        const isLocked = profileElement.querySelector('input').checked;
+
+        if (!isLocked) {
+            const privateInfoElement = profileElement.querySelector('div');
+
+            if (e.target.textContent == 'Show more') {
+                privateInfoElement.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else if (e.target.textContent == 'Hide it') {
+                privateInfoElement.style.display = 'none';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
