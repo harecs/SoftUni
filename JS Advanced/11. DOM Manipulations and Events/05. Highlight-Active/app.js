4f07d9bb3afb6f1b330f79a07fc517c34a4a5077
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 10:09:30 +0300

    Finish "05. Highlight Active"

--- JS Advanced/11. DOM Manipulations and Events/05. Highlight-Active/app.js
+++ JS Advanced/11. DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    const inputElements = Array.from(document.querySelectorAll('input'));
+    
+    inputElements.forEach(ele => ele.addEventListener('focus', onFocus));
+    inputElements.forEach(ele => ele.addEventListener('blur', onBlur));
+
+    function onFocus(e) {
+        e.target.parentElement.className = 'focused';
+    }
+
+    function onBlur(e) {
+        e.target.parentElement.className = '';
+    }
 }
