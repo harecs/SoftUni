a31af71e0c496c4628910b8403103fadccb836a4
Zahari Lazarov <harilazaro@gmail.com>
2023-09-30 09:04:43 +0300

    Finish "02. Delete from Table"

--- JS Advanced/11. DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ JS Advanced/11. DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const emailToDelete = document.querySelector('label input').value;
+    const emailElements = document.querySelectorAll('td:nth-child(even)');
+    const resultElement = document.querySelector('#result');
+
+    for (const element of emailElements) {
+        if (element.textContent == emailToDelete) {
+            element.parentNode.remove();
+            resultElement.textContent = 'Deleted.';
+            return;
+        }
+    }
+
+    resultElement.textContent = 'Not found.';
 }
