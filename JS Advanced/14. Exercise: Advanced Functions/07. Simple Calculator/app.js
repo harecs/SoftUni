e2c7a346520335c5ee037cbe9c77b7bd8691761c
Zahari Lazarov <harilazaro@gmail.com>
2023-10-04 12:58:04 +0300

    Finish "07. Simple Calculator"

--- JS Advanced/14. Exercise: Advanced Functions/07. Simple Calculator/app.js
+++ JS Advanced/14. Exercise: Advanced Functions/07. Simple Calculator/app.js
@@ -1,7 +1,20 @@
 function calculator() {
-    // TODO:
+    return {
+        firstNumElement: null,
+        secondNumElement: null,
+        resultElement: null,
+        init: (firstNumSelector, secondNumSelector, resultSelector) => {
+
+            this.firstNumElement = document.querySelector(firstNumSelector);
+            this.secondNumElement = document.querySelector(secondNumSelector);
+            this.resultElement = document.querySelector(resultSelector);
+        },
+        add: () => {
+            debugger
+            this.resultElement.value = Number(this.firstNumElement.value) + Number(this.secondNumElement.value);
+        },
+        subtract: () => {
+            this.resultElement.value = Number(this.firstNumElement.value) - Number(this.secondNumElement.value);
+        }
+    }
 }
-
-
-
-
