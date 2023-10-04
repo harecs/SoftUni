cbcd2d5fc41753f237a3d9d7db95921b7aa0c2a6
Zahari Lazarov <harilazaro@gmail.com>
2023-10-04 13:12:16 +0300

    Finish "08. Next Article"

--- JS Advanced/14. Exercise: Advanced Functions/08. Next Article/script.js
+++ JS Advanced/14. Exercise: Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,11 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(inputArray) {
+    const articles = inputArray.slice();
+
+    return function () {
+        if (articles.length > 0) {
+            const articleElement = document.createElement('article');
+            articleElement.textContent = articles.shift();
+            document.querySelector('#content').appendChild(articleElement);
+        }
+    }
 }
