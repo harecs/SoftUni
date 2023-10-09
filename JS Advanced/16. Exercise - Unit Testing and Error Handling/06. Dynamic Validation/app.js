575de31028f8869627ee5258d82c28c8acec1b0c
Zahari Lazarov <harilazaro@gmail.com>
2023-10-09 18:30:35 +0300

    Finish "06. Dynamic Validation"

--- JS Advanced/16. Exercise - Unit Testing and Error Handling/06. Dynamic Validation/app.js
+++ JS Advanced/16. Exercise - Unit Testing and Error Handling/06. Dynamic Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    const inputFieldElement = document.querySelector('#email');
+    const regExp = /[a-z]+@[a-z]+.[a-z]+/g;
+
+    inputFieldElement.addEventListener('change', (e) => {
+        if (!regExp.test(inputFieldElement.value)) {
+            inputFieldElement.className = 'error';
+        } else {
+            inputFieldElement.className = '';
+        }
+    });
 }
