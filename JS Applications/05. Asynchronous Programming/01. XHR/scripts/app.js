0424f4472b6b1f93ef23c4b978c2df1a6a93b9e0
Zahari Lazarov <harilazaro@gmail.com>
2023-11-06 16:25:50 +0200

    Finish "01. XHR"

--- JS Applications/05. Asynchronous Programming/01. XHR/scripts/app.js
+++ JS Applications/05. Asynchronous Programming/01. XHR/scripts/app.js
@@ -1,3 +1,12 @@
 function loadRepos() {
-   console.log("TODO...");
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState == 4 && request.status == 200) {
+            document.querySelector('#res').textContent = request.responseText;
+        }
+    });
+
+    request.open('GET', 'https://api.github.com/users/testnakov/repos');
+    request.send();
 }
