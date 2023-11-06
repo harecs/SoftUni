071939f56ee7063ae018102901be329bfd6c2ce9
Zahari Lazarov <harilazaro@gmail.com>
2023-11-06 16:51:39 +0200

    Finish "02. GitHub Repos"

--- JS Applications/05. Asynchronous Programming/02. GitHub Repos/scripts/app.js
+++ JS Applications/05. Asynchronous Programming/02. GitHub Repos/scripts/app.js
@@ -1,3 +1,42 @@
 function loadRepos() {
-	console.log("TODO...");
+	const url = 'https://api.github.com';
+	const reposListElement = document.querySelector('#repos');
+	const username = document.querySelector('#username').value;
+
+	fetch(url + `/users/${username}/repos`)
+		.then(res => {
+			if (res.ok) {
+				return res.json()
+			}
+
+			throw new Error(res.statusText);
+		})
+		.then(data => {
+			resetReposList();
+
+			Array.from(data).forEach(repo => {
+				const aElement = document.createElement('a');
+				aElement.textContent = repo['full_name'];
+				aElement.setAttribute('href', repo['html_url']);
+
+				const liElement = document.createElement('li');
+				liElement.appendChild(aElement);
+
+				reposListElement.appendChild(liElement);
+			});
+		})
+		.catch(err => {
+			resetReposList();
+			const aElement = document.createElement('a');
+			aElement.textContent = username;
+
+			const liElement = document.createElement('li');
+			liElement.appendChild(aElement);
+
+			reposListElement.appendChild(liElement)
+		});
+
+	function resetReposList() {
+		Array.from(reposListElement.children).forEach(child => child.remove());
+	}
 }
