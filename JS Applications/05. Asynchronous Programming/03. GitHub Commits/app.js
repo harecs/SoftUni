2e6a964e2505299a31fbb5691e9caa20a1415cf3
Zahari Lazarov <harilazaro@gmail.com>
2023-11-06 17:15:05 +0200

    Finish "03. GitHub Commits"

--- JS Applications/05. Asynchronous Programming/03. GitHub Commits/app.js
+++ JS Applications/05. Asynchronous Programming/03. GitHub Commits/app.js
@@ -1,4 +1,36 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const url = 'https://api.github.com';
+    const username = document.querySelector('#username').value;
+    const repo = document.querySelector('#repo').value;
+    const commitsListElement = document.querySelector('#commits');
+
+    fetch(url + `/repos/${username}/${repo}/commits`, { method: 'GET' })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+
+            throw new Error(res.status);
+        })
+        .then(data => {
+            data.forEach(commitInfo => {
+                const liElement = createLiItem(`${commitInfo.commit.author.name}: ${commitInfo.commit.message}`);
+                commitsListElement.appendChild(liElement);
+            });
+        })
+        .catch((err) => {
+            removeCommitsListChildren();
+            commitsListElement.appendChild(createLiItem(`${err} (Not Found)`));
+        })
+
+    function removeCommitsListChildren() {
+        Array.from(commitsListElement.children).forEach(ele => ele.remove());
+    }
+
+    function createLiItem(textContent) {
+        const liElement = document.createElement('li');
+        liElement.textContent = textContent;
+
+        return liElement;
+    }
 }
