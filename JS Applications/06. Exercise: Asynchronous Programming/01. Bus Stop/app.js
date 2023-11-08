ddba28ba96925884ccb985baf7b6f3f829bfeeb2
Zahari Lazarov <harilazaro@gmail.com>
2023-11-08 10:26:25 +0200

    Finish "01. Bus Stop"

--- JS Applications/06. Exercise: Asynchronous Programming/01. Bus Stop/app.js
+++ JS Applications/06. Exercise: Asynchronous Programming/01. Bus Stop/app.js
@@ -1,3 +1,42 @@
+getInfo();
+
 function getInfo() {
-    console.log("TODO...");
+    const url = 'http://localhost:3030';
+    const stopIdInputEle = document.querySelector('#stopId');
+    const checkButtonEle = document.querySelector('#submit');
+    const stopNameEle = document.querySelector('#stopName');
+    const busesListEle = document.querySelector('#buses');
+
+    checkButtonEle.addEventListener('click', onCheck);
+
+    function onCheck(e) {
+        fetch(url + `/jsonstore/bus/businfo/${stopIdInputEle.value}`, { method: 'GET' })
+            .then(res => {
+                if (res.status == 200) {
+                    return res.json();
+                }
+
+                throw new Error(res.statusText);
+            })
+            .then(data => {
+                clearBUsesList();
+                stopNameEle.textContent = data.name;
+
+                Object.entries(data.buses).forEach(busEntry => {
+                    const [busId, time] = busEntry;
+                    const liEle = document.createElement('li');
+                    liEle.textContent = `Bus ${busId} arrives in ${time} minutes`;
+
+                    busesListEle.appendChild(liEle);
+                });
+            })
+            .catch(err => {
+                clearBUsesList();
+                stopNameEle.textContent = 'Error'
+            });
+    }
+
+    function clearBUsesList() {
+        Array.from(busesListEle.children).forEach(child => child.remove());
+    }
 }
