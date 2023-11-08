0ad6d01fe88f47f1709c1c3a7a0dfe64a39f2cd5
Zahari Lazarov <harilazaro@gmail.com>
2023-11-08 11:49:39 +0200

    Finish "02. Bus Schedule"

--- JS Applications/06. Exercise: Asynchronous Programming/02. Bus Schedule/app.js
+++ JS Applications/06. Exercise: Asynchronous Programming/02. Bus Schedule/app.js
@@ -1,11 +1,44 @@
 function solve() {
+    let nextStopId = 'depot';
+    let currentStopName = '';
+    const departButtonEle = document.querySelector('#depart');
+    const arriveButtonEle = document.querySelector('#arrive');
+    const infoEle = document.querySelector('.info');
+
+    const disableDepartButton = () => departButtonEle.disabled = true;
+    const disableArriveButton = () => arriveButtonEle.disabled = true;
+    const enableDepartButton = () => departButtonEle.disabled = false;
+    const enableArriveButton = () => arriveButtonEle.disabled = false;
 
     function depart() {
-        console.log('Depart TODO...');
+        const url = 'http://localhost:3030';
+        fetch(url + `/jsonstore/bus/schedule/${nextStopId}`, { method: 'GET' })
+            .then(res => {
+                if (res.status == 200) {
+                    return res.json();
+                }
+
+                throw new Error(res.statusText);
+            })
+            .then(data => {
+                currentStopName = data.name;
+                nextStopId = data.next;
+
+                infoEle.textContent = `Next stop ${currentStopName}`;
+                disableDepartButton();
+                enableArriveButton();
+            })
+            .catch(err => {
+                disableDepartButton();
+                disableArriveButton();
+                infoEle.textContent = 'Error'
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoEle.textContent = `Arriving at ${currentStopName}`;
+        disableArriveButton();
+        enableDepartButton();
     }
 
     return {
