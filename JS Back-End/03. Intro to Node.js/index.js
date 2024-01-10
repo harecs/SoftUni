afdc2e48a90520f448ea98a60d1a14d816ec95ec
Zahari Lazarov <harilazaro@gmail.com>
2024-01-10 15:01:32 +0200

    Finish and refactor the Node.js server

--- JS Back-End/03. Intro to Node.js/index.js
+++ JS Back-End/03. Intro to Node.js/index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const port = 5001;
+
 const siteCss = require('./styles/site.css');
 const homeHtml = require('./views/index.html');
 const addBreedHtml = require('./views/addBreed.html');
@@ -9,120 +11,71 @@ const catShelterHtml = require('./views/catShelter.html');
 const breeds = require('./data/breeds');
 const cats = require('./data/cats');
 
-const port = 5001;
-
 const server = http.createServer((req, res) => {
-    const url = new URL(req.headers.host + req.url);
-    
-    console.log('New request to ', req.url);
-    console.log('-- HTTP Method ', req.method);
-    console.log(url.searchParams);
+    console.log(`Request (${req.method}) ${req.url}`);
 
-    if (url.pathname.includes('site.css')) {
-        res.writeHead(200, {
-            'Content-Type': 'text/css'
-        });
+    if (req.method == 'GET') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+    } else if (req.method == 'POST') {
+        res.statusCode = 301
+        res.setHeader('Location', '/');
+    }
 
+    const paths = req.url.split('/');
+    const catId = Number(paths[paths.length - 1]);
+    const catArrayIndex = cats.findIndex(cat => cat.id == catId);
+
+    req.on('data', (chunk) => {
+        const data = Object
+            .fromEntries(new URL(`${req.headers.host}${req.url}/?${chunk.toString()}`)
+                .searchParams
+                .entries());
+
+        const { name, description, upload, breed } = data;
+
+        if (req.url == '/cats/add-breed') {
+            breeds.push(breed);
+        } else if (req.url == '/cats/add-cat') {
+            const id = cats[0] ? cats[cats.length - 1].id + 1 : 0;
+            cats.push({ id, name, description, image: upload, breed });
+        } else if (req.url.includes('cats/edit-cat/')) {
+            cats[catArrayIndex] = { id: catId, name, description, image: upload, breed };
+        }
+    });
+
+    if (req.url.includes('site.css')) {
+        res.setHeader('Content-Type', 'text/css');
         res.write(siteCss);
     } else if (req.url == '/') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
+        res.write(homeHtml(cats));
+    } else if (req.url.includes('/search?search')) {
+        const url = new URL(`${req.headers.host}${req.url}`);
+        const search = url.searchParams.get('search');
+
+        const searchedCats = cats.filter(cat => {
+            return cat.name.includes(search) || cat.description.includes(search) || cat.breed.includes(search)
         });
 
-        res.write(homeHtml(cats));
-    } else if (url.pathname == `${port}/cats/add-breed`) {
-        if (url.search) {
-            breeds.push(url.searchParams.get('breed'));
-
-            res.writeHead(301, {
-                'Location': '/'
-            });
-
-            res.write(homeHtml(cats));
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-
-            res.write(addBreedHtml);
-        }
-    } else if (url.pathname == `${port}/cats/add-cat`) {
-        if (url.search) {
-            const cat = {
-                id: cats[0] ? cats[cats.length - 1].id + 1 : 0,
-                name: url.searchParams.get('name'),
-                description: url.searchParams.get('description'),
-                image: url.searchParams.get('upload'),
-                breed: url.searchParams.get('breed')
-            }
-
-            cats.push(cat);
-
-            res.writeHead(301, {
-                'Location': '/'
-            });
-
-            res.write(homeHtml(cats));
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-
-            res.write(addCatHtml(breeds));
-        }
-    } else if (url.pathname.includes('/cats/edit-cat')) {
-        const paths = url.pathname.split('/');
-        const catId = Number(paths[paths.length - 1].split('?')[0]);
-        const catArrayIndex = cats.findIndex(cat => cat.id == catId);
-
-        if (url.search) {
-            cats[catArrayIndex] = {
-                id: catId,
-                name: url.searchParams.get('name'),
-                description: url.searchParams.get('description'),
-                image: url.searchParams.get('upload'),
-                breed: url.searchParams.get('breed')
-            }
-
-            res.writeHead(301, {
-                'Location': '/'
-            });
-
-            res.write(homeHtml(cats));
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-
-            res.write(editCatHtml(cats[catArrayIndex], breeds));
-        }
-    } else if (url.pathname.includes('/cats/view-cat')) {
-        const paths = url.pathname.split('/');
-        const catId = Number(paths[paths.length - 1]);
-        const catArrayIndex = cats.findIndex(cat => cat.id == catId);
-
+        res.write(homeHtml(searchedCats));
+    } else if (req.url == '/cats/add-breed') {
+        res.statusCode = 200;
+        res.write(addBreedHtml);
+    } else if (req.url == '/cats/add-cat') {
+        req.method == 'GET' ? res.write(addCatHtml(breeds)) : res.write(homeHtml(cats));
+    } else if (req.url.includes('/cats/edit-cat/')) {
+        req.method == 'GET'
+            ? res.write(editCatHtml(cats[catArrayIndex], breeds))
+            : res.write(homeHtml(cats));
+    } else if (req.url.includes('/cats/view-cat/')) {
         if (req.method == 'POST') {
             cats.splice(catArrayIndex, 1);
-
-            console.log(cats);
-
-            res.writeHead(301, {
-                'Location': '/'
-            });
-
             res.write(homeHtml(cats));
         } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-
             res.write(catShelterHtml(cats[catArrayIndex]));
         }
     } else {
-        res.writeHead(404, {
-            'Content-Type': 'text/html'
-        });
-
+        res.statusCode = 404;
         res.write('<h1>404 Not Found</h1>');
     }
 
