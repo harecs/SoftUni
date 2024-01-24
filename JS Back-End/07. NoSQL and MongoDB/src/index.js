e692a24056a8a409c4c207cd978952103ff70184
Zahari Lazarov <harilazaro@gmail.com>
2024-01-24 15:34:44 +0200

    Make READ operations

--- JS Back-End/07. NoSQL and MongoDB/src/index.js
+++ JS Back-End/07. NoSQL and MongoDB/src/index.js
@@ -4,6 +4,7 @@ const Person = require('./models/Person');
 
 mongoose.connect('mongodb://localhost:27017/haridb');
 
+// CREATE
 // Person.create({
 //     firstName: 'Stamat',
 //     lastName: 'Petrov',
@@ -13,14 +14,30 @@ mongoose.connect('mongodb://localhost:27017/haridb');
 //     console.log(result);
 // });
 
-const person = new Person({
-    firstName: 'Pesho',
-    lastName: 'Mitkov',
-    age: 15,
-    email: 'pesho5000@gmail.com'
-});
+// const person = new Person({
+//     firstName: 'Stamat',
+//     lastName: 'Petrov',
+//     age: 39,
+//     email: 'stamat@stamatpetrov-bg.info'
+// });
 
-person.save()
-    .then(result => {
-        console.log(result);
+// person.save()
+//     .then(result => {
+//         console.log(result);
+//     });
+
+// READ
+Person.find({ lastName: 'Petrov' })
+    .then(people => {
+        people.forEach(person => person.logInfo());
+    });
+
+Person.findOne({ lastName: 'Petrov', age: { $lte: 35 } })
+    .then(person => {
+        console.log(person.email);
+    });
+
+Person.findById('65b0fa06982d7e3a185149b0')
+    .then(person => {
+        console.log(person);
     });
