48076b85cf76ecd61843cc4abe7357600d63f1fa
Zahari Lazarov <harilazaro@gmail.com>
2024-01-24 13:53:30 +0200

    Add another way to create a person

--- JS Back-End/07. NoSQL and MongoDB/src/index.js
+++ JS Back-End/07. NoSQL and MongoDB/src/index.js
@@ -4,11 +4,23 @@ const Person = require('./models/Person');
 
 mongoose.connect('mongodb://localhost:27017/haridb');
 
-Person.create({
-    firstName: 'Stamat',
-    lastName: 'Petrov',
-    age: 39,
-    email: 'stamat@stamatpetrov-bg.info'
-}).then((result) => {
-    console.log(result);
+// Person.create({
+//     firstName: 'Stamat',
+//     lastName: 'Petrov',
+//     age: 39,
+//     email: 'stamat@stamatpetrov-bg.info'
+// }).then((result) => {
+//     console.log(result);
+// });
+
+const person = new Person({
+    firstName: 'Pesho',
+    lastName: 'Mitkov',
+    age: 15,
+    email: 'pesho5000@gmail.com'
 });
+
+person.save()
+    .then(result => {
+        console.log(result);
+    });
