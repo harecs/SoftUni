ce1e3e0b8cb032d2db3efd084ceb50cd8ed99657
Zahari Lazarov <harilazaro@gmail.com>
2024-02-20 12:24:57 +0200

    Rewrite 01

--- JS Advanced (TS)/03. Syntax, Functions and Statements/01-echoFunction.js
+++ JS Advanced (TS)/03. Syntax, Functions and Statements/01-echoFunction.js
@@ -2,3 +2,5 @@ function echo(input) {
     console.log(input.length);
     console.log(input);
 }
+echo('Hello, JavaScript!');
+echo('strings are easy');
---
+++ JS Advanced (TS)/03. Syntax, Functions and Statements/01-echoFunction.ts
@@ -0,0 +1,7 @@
+function echo(input: string) {
+    console.log(input.length);
+    console.log(input);
+}
+
+echo('Hello, JavaScript!');
+echo('strings are easy');
