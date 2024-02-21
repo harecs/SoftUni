1bcd9642d6bba0c9de09bde098f36794b5b6b48d
Zahari Lazarov <harilazaro@gmail.com>
2024-02-21 15:59:29 +0200

    04. Object Factory with TypeScript

---
+++ JS Advanced (TS)/07. Objects and Composition/src/04. Object Factory.ts
@@ -0,0 +1,55 @@
+interface OrderInterface {
+    template: object,
+    parts: string[]
+}
+
+function factory(library: object, orders: OrderInterface[]): object[] {
+    const result: object[] = [];
+
+    for (const order of orders) {
+        const readyOrder = Object.assign({}, order.template);
+
+        order.parts.forEach(part => {
+            readyOrder[part] = library[part];
+        });
+
+        result.push(readyOrder);
+    }
+
+    return result;
+}
+
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+
+};
+
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
+
+const products = factory(library, orders);
+console.table(products);
