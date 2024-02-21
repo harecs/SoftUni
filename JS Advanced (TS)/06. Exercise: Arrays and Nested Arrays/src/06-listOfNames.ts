1698f2ad78d0ac1e3b70b209e810313456db4158
Zahari Lazarov <harilazaro@gmail.com>
2024-02-21 12:25:39 +0200

    List of names with TypeScript

--- JS Advanced (TS)/06. Exercise: Arrays and Nested Arrays/src/06-listOfNames.ts
+++ JS Advanced (TS)/06. Exercise: Arrays and Nested Arrays/src/06-listOfNames.ts
@@ -1,6 +1,10 @@
-function sortNames(names) {
+type Name = string;
+
+function sortNames(names: Name[]): void {
     names.sort((a, b) => a.localeCompare(b));
     names.forEach((name, i) => {
         console.log(`${i + 1}.${name}`);
     });
 }
+
+sortNames(["John", "Bob", "Christina", "Ema"]);
