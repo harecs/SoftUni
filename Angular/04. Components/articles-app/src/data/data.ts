8239d08aa7e4e4a845786a223ee73a5ea612dbf6
Zahari Lazarov <harilazaro@gmail.com>
2024-02-22 10:03:02 +0200

    Create ArticleData class

--- Angular/04. Components/articles-app/src/data/data.ts
+++ Angular/04. Components/articles-app/src/data/data.ts
@@ -1,2 +1,17 @@
 import { Article } from "src/models/article.model";
+import { data } from "./seed";
 
+export class ArticleData {
+    getData(): Article[] {
+        const articles: Article[] = [];
+
+        for (const obj of data) {
+            const { title, description, author, imageUrl } = obj;
+            const article = new Article(title, description, author, imageUrl);
+
+            articles.push(article);
+        }
+
+        return articles;
+    }
+}
