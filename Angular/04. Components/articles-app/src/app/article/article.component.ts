43987c6ca41a917f353838baa7f39546e974ef2e
Zahari Lazarov <harilazaro@gmail.com>
2024-02-22 10:27:06 +0200

    Add some code

---
+++ Angular/04. Components/articles-app/src/app/article/article.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from "@angular/core";
+import { Article } from "../models/article.model";
+
+@Component({
+    selector: 'app-article',
+    templateUrl: './article.component.html',
+    styleUrls: ['./article.component.css']
+})
+export class ArticleComponent {
+    private symbols: number = 250;
+    // @Input() article: Article;
+    // @Input() articleDesc: string;
+    descToShow: string;
+    articleDescLen: number;
+    showReadMoreBtn: boolean = true;
+    showHideBtn: boolean = false;
+    imageIsShown: boolean = false;
+    imageButtonTitle: string = 'Show Image';
+
+    constructor() {
+        this.articleDescLen = 0;
+        this.descToShow = '';
+    }
+}
