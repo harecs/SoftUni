84345d6e71308f8fd97242248f2bf526008d1ca1
Zahari Lazarov <harilazaro@gmail.com>
2024-03-04 15:59:46 +0200

    Add posts and themes list components

--- Angular/06. Workshop - Components/workshop-components/src/app/app.module.ts
+++ Angular/06. Workshop - Components/workshop-components/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PostsListComponent,
+    ThemesListComponent
   ],
   imports: [
     BrowserModule,
