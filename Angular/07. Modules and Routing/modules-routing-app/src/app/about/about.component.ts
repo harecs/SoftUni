e174d28ac84f55eb77f3d70f0c46506d82574c73
Zahari Lazarov <harilazaro@gmail.com>
2024-03-06 15:12:10 +0200

    Fetch photos from API

--- Angular/07. Modules and Routing/modules-routing-app/src/app/about/about.component.ts
+++ Angular/07. Modules and Routing/modules-routing-app/src/app/about/about.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Photo } from '../types/photo';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  private URL: string = `https://jsonplaceholder.typicode.com/photos`;
+  photos: Photo[] | null = null;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(`${this.URL}?_start=0&_limit=4`)
+      .subscribe(photos => {
+        this.photos = photos as Photo[];
+      })
+  }
 }
--- Angular/07. Modules and Routing/modules-routing-app/src/app/app.module.ts
+++ Angular/07. Modules and Routing/modules-routing-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CoreModule
   ],
   providers: [],
