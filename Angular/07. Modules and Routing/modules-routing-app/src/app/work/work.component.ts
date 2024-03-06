fbfa3c8466707aa21382b83f16f32279cf11864b
Zahari Lazarov <harilazaro@gmail.com>
2024-03-06 16:24:51 +0200

    Work page

--- Angular/07. Modules and Routing/modules-routing-app/src/app/work/work.component.ts
+++ Angular/07. Modules and Routing/modules-routing-app/src/app/work/work.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JsonPlaceholderService } from '../jsonplaceholder.service';
+import { Observable } from 'rxjs';
+
+import { Photo } from '../types/photo';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.css']
 })
-export class WorkComponent {
+export class WorkComponent implements OnInit {
+  photosSubscription: Observable<Photo[]> = this.JsonPlaceholderService.getPhotos(9, 8);
+  photos1: Photo[] = [];
+  photos2: Photo[] = [];
 
+  isModalOpened: boolean = false;
+  modalSrc: string = '';
+  modalCaption: string = '';
+
+  constructor(private JsonPlaceholderService: JsonPlaceholderService) { }
+
+  ngOnInit(): void { 
+      this.photosSubscription.subscribe((photos) => {
+        this.photos1 = photos.slice(0, 4);
+        this.photos2 = photos.slice(4, 8);        
+      });
+  }
+
+  onPhotoClick(event: MouseEvent): void {
+    const photoElement = event.target as HTMLImageElement;
+
+    this.modalSrc = photoElement.src;
+    this.modalCaption = photoElement.alt;
+
+    this.isModalOpened = true;
+  }
+
+  onModalClick(): void {
+    this.isModalOpened = false;
+  }
 }
